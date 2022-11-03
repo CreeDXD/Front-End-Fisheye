b20f336ae62980818c9e8c088ef4ba0df17e85d9
CreeDXD <pclio92@gmail.com>
2022-11-03 12:25:46 +0100

    fetch la base de donnée et ajout info des photographes

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait,city,country,price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,14 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const villePays = document.createElement('p');
+        villePays.textContent = city +','+country;
+        const prix = document.createElement('p');
+        prix.textContent = price;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(villePays);
+        article.appendChild(prix);
         return (article);
     }
     return { name, picture, getUserCardDOM }

f88d55eec3b9354813c4ba5f5e0c9ff976e5fef7
CreeDXD <pclio92@gmail.com>
2022-12-19 19:05:57 +0100

    odification de finalisation de projet

--- scripts/templets/PhotographerCard.js
+++ scripts/templets/PhotographerCard.js
@@ -19,7 +19,7 @@ class PhotographerCard {
             <h2>${this._photographer.name}</h2>
         </a>
             
-        <h3>${this._photographer.city}${this._photographer.country}</h3>
+        <h3>${this._photographer.city}, ${this._photographer.country}</h3>
         <p>${this._photographer.tagline}</p>
         <p>${this._photographer.price}€/jour</p>            
         `
